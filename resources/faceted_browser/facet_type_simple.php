4138c82dfe6e71fb650892c9b37fd95e018eb958
Jesse McCarthy <git_commits@jessemccarthy.net>
2015-03-31 16:10:57 -0400

    Change license.

--- resources/faceted_browser/facet_type_simple.php
+++ resources/faceted_browser/facet_type_simple.php
@@ -1,26 +1,5 @@
 <?php
 
-/*
-
-Copyright © 2009-2010 Jesse McCarthy <http://jessemccarthy.net/>
-
-This file is part of PHP Faceted Browser (the "Software").
-
-The Software is free software: you can redistribute it and/or modify
-it under the terms of the GNU General Public License as published by
-the Free Software Foundation, either version 3 of the License, or
-(at your option) any later version.
-
-The Software is distributed in the hope that it will be useful,
-but WITHOUT ANY WARRANTY; without even the implied warranty of
-MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
-GNU General Public License for more details.
-
-You should have received a copy of the GNU General Public License
-along with the Software.  If not, see <http://www.gnu.org/licenses/>.
-
-*/
-
 class Facet_Type_Simple extends Facet {
 
   /**
